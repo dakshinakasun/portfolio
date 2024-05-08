1f0e72d9481ebbc8f83a991898f37ecf8e6124bb
dakshinakasun <dakshinakasun20@gmail.com>
2024-05-08 16:18:48 +0530

    error fix 1.3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,40 +12,43 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* <div className="h-full overflow-scroll no-scrollbar flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48"> */}
+      <div className="h-full overflow-scroll no-scrollbar flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative pb-4">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
 
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-          {/* TITLE */}
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
-          </h1>
+        <div className="h-1/2 lg:h-full lg:w-1/2 lg:flex lg:flex-col lg:gap-8 items-center justify-center">
+          <div className="flex flex-col gap-8">
+            {/* TITLE */}
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+              Crafting Digital Experiences, Designing Tomorrow.
+            </h1>
 
-          {/* DESC */}
-          <p className="md:text-base">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
-          </p>
+            {/* DESC */}
+            <p className="md:text-base">
+              Welcome to my digital canvas, where innovation and creativity
+              converge. With a keen eye for aesthetics and a mastery of code, my
+              portfolio showcases a diverse collection of projects that reflect
+              my commitment to excellence.
+            </p>
 
-          {/* BUTTONS */}
-          <div className="w-full flex gap-4">
-            <Link href="/portfolio">
-              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
-                View My Work
-              </button>
-            </Link>
+            {/* BUTTONS */}
+            <div className="w-full flex gap-4 pb-5">
+              <Link href="/portfolio">
+                <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+                  View My Work
+                </button>
+              </Link>
 
-            <Link href="/contact">
-              <button className="p-4 rounded-lg ring-1 ring-black">
-                Contact Me
-              </button>
-            </Link>
+              <Link href="/contact">
+                <button className="p-4 rounded-lg ring-1 ring-black">
+                  Contact Me
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
