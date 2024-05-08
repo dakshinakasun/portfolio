fd5ebc87388eb45b9e93739ded65c70880b230b2
dakshinakasun <dakshinakasun20@gmail.com>
2024-05-08 16:25:20 +0530

    error fix 1.3

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -43,7 +43,7 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full overflow-scroll no-scrollbar flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
         <div className=" h-1/4 lg:h-full lg:w-1/2 flex items-center justify-center text-4xl md:text-5xl lg:text-6xl py-5">
           <div className="">
